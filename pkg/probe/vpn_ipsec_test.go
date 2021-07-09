511566bce0182e8778f1f2ac0194a9ffa7a80234
Christian Svensson <blue@cmd.nu>
2021-07-09 12:31:21 +0200

    minor(tests): Test behaviour on common phase2 IDs
    This tests that issue #96 was fixed by #86.

--- pkg/probe/vpn_ipsec_test.go
+++ pkg/probe/vpn_ipsec_test.go
@@ -36,3 +36,43 @@ func TestVPNIPSec(t *testing.T) {
 		t.Fatalf("metric compare: err %v", err)
 	}
 }
+
+func TestVPNIPSecWithCommonP2Names(t *testing.T) {
+	c := newFakeClient()
+	c.prepare("api/v2/monitor/vpn/ipsec", "testdata/ipsec-common-p2.jsonnet")
+	r := prometheus.NewPedanticRegistry()
+	if !testProbe(probeVPNIPSec, c, r) {
+		t.Errorf("probeVPNIPSec() returned non-success")
+	}
+
+	em := `
+	# HELP fortigate_ipsec_tunnel_receive_bytes_total Total number of bytes received over the IPsec tunnel
+	# TYPE fortigate_ipsec_tunnel_receive_bytes_total counter
+	fortigate_ipsec_tunnel_receive_bytes_total{name="CommonP2",p2serial="22",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_receive_bytes_total{name="CommonP2",p2serial="23",parent="My VPN",vdom="root"} 4.782292004e+09
+	fortigate_ipsec_tunnel_receive_bytes_total{name="CommonP2",p2serial="24",parent="My VPN",vdom="root"} 3.82868846e+08
+	fortigate_ipsec_tunnel_receive_bytes_total{name="CommonP2",p2serial="25",parent="My VPN",vdom="root"} 1.581264e+06
+	fortigate_ipsec_tunnel_receive_bytes_total{name="mgmt",p2serial="1",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_receive_bytes_total{name="some-network",p2serial="14",parent="My VPN",vdom="root"} 274832
+	# HELP fortigate_ipsec_tunnel_transmit_bytes_total Total number of bytes transmitted over the IPsec tunnel
+	# TYPE fortigate_ipsec_tunnel_transmit_bytes_total counter
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="CommonP2",p2serial="22",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="CommonP2",p2serial="23",parent="My VPN",vdom="root"} 1.57533239e+09
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="CommonP2",p2serial="24",parent="My VPN",vdom="root"} 5.53928639e+08
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="CommonP2",p2serial="25",parent="My VPN",vdom="root"} 3.1269542e+07
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="mgmt",p2serial="1",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_transmit_bytes_total{name="some-network",p2serial="14",parent="My VPN",vdom="root"} 112307
+	# HELP fortigate_ipsec_tunnel_up Status of IPsec tunnel
+	# TYPE fortigate_ipsec_tunnel_up gauge
+	fortigate_ipsec_tunnel_up{name="CommonP2",p2serial="22",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_up{name="CommonP2",p2serial="23",parent="My VPN",vdom="root"} 1
+	fortigate_ipsec_tunnel_up{name="CommonP2",p2serial="24",parent="My VPN",vdom="root"} 1
+	fortigate_ipsec_tunnel_up{name="CommonP2",p2serial="25",parent="My VPN",vdom="root"} 1
+	fortigate_ipsec_tunnel_up{name="mgmt",p2serial="1",parent="My VPN",vdom="root"} 0
+	fortigate_ipsec_tunnel_up{name="some-network",p2serial="14",parent="My VPN",vdom="root"} 1
+	`
+
+	if err := testutil.GatherAndCompare(r, strings.NewReader(em)); err != nil {
+		t.Fatalf("metric compare: err %v", err)
+	}
+}
