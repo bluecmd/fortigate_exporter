dde9e6cea1a77100c1f9ca6319e38c71e3deac63
Gianni Stubbe <gianni.stubbe@citymesh.com>
2023-04-18 12:03:29 +0200

    feat(Fortimanager): Change fortimanager status to return as value
    Fortimanager status will need to be requested by value instead of label

--- pkg/probe/system_fortimanager.go
+++ pkg/probe/system_fortimanager.go
@@ -22,13 +22,13 @@ func probeSystemFortimanagerStatus(c http.FortiHTTP, meta *TargetMetadata) ([]pr
 	var (
 		FortimanStat_id = prometheus.NewDesc(
 			"fortigate_fortimanager_connection_status",
-			"Fortimanager status ID",
-			[]string{"vdom", "mode", "status"}, nil,
+			"Fortimanager tunnel status (0 - Down, 1 - Handshake in progress, 2 - Up)",
+			[]string{"vdom", "mode"}, nil,
 		)
 		FortimanReg_id = prometheus.NewDesc(
 			"fortigate_fortimanager_registration_status",
-			"Fortimanager registration status ID",
-			[]string{"vdom", "mode", "status"}, nil,
+			"Fortimanager registration status (0 - Unknown Registration, 1 - In Progress, 2 - Registered, 3 - Registered but Unauthorized)",
+			[]string{"vdom", "mode"}, nil,
 		)
 	)
 
@@ -40,50 +40,8 @@ func probeSystemFortimanagerStatus(c http.FortiHTTP, meta *TargetMetadata) ([]pr
 
 	m := []prometheus.Metric{}
 	for _, r := range res {
-		StatusDown, StatusHandshake, StatusUp := 0.0, 0.0, 0.0
-		switch r.Results.Status_ID {
-		case 0:
-			// No management Tunnel
-			StatusDown = 1.0
-			break
-		case 1:
-			// Management tunnel establishment in progress
-			StatusHandshake = 1.0
-			break
-		case 2:
-			// Management tunnel is establised
-			StatusUp = 1.0
-			break
-		}
-
-		RegistrationUnknown, RegistrationInProgress, RegistrationRegistered, RegistrationUnregistered := 0.0, 0.0, 0.0, 0.0
-		switch r.Results.Registration_ID {
-		case 0:
-			// FMG does not know about the device
-			RegistrationUnknown = 1.0
-			break
-		case 1:
-			// FMG does know the device, but it is not yet fully saved in the list of unregistered devices
-			RegistrationInProgress = 1.0
-			break
-		case 2:
-			// FMG does know the device, and device is authorized
-			RegistrationRegistered = 1.0
-			break
-		case 3:
-			// FMG does know the device, but it is not yet authorized
-			RegistrationUnregistered = 1.0
-			break
-		}
-
-		m = append(m, prometheus.MustNewConstMetric(FortimanStat_id, prometheus.GaugeValue, StatusDown, r.VDOM, r.Results.Mode, "down"))
-		m = append(m, prometheus.MustNewConstMetric(FortimanStat_id, prometheus.GaugeValue, StatusHandshake, r.VDOM, r.Results.Mode, "handshake"))
-		m = append(m, prometheus.MustNewConstMetric(FortimanStat_id, prometheus.GaugeValue, StatusUp, r.VDOM, r.Results.Mode, "up"))
-
-		m = append(m, prometheus.MustNewConstMetric(FortimanReg_id, prometheus.GaugeValue, RegistrationUnknown, r.VDOM, r.Results.Mode, "unknown"))
-		m = append(m, prometheus.MustNewConstMetric(FortimanReg_id, prometheus.GaugeValue, RegistrationInProgress, r.VDOM, r.Results.Mode, "inprogress"))
-		m = append(m, prometheus.MustNewConstMetric(FortimanReg_id, prometheus.GaugeValue, RegistrationRegistered, r.VDOM, r.Results.Mode, "registered"))
-		m = append(m, prometheus.MustNewConstMetric(FortimanReg_id, prometheus.GaugeValue, RegistrationUnregistered, r.VDOM, r.Results.Mode, "unregistered"))
+		m = append(m, prometheus.MustNewConstMetric(FortimanStat_id, prometheus.GaugeValue, float64(r.Results.Status_ID), r.VDOM, r.Results.Mode))
+		m = append(m, prometheus.MustNewConstMetric(FortimanReg_id, prometheus.GaugeValue, float64(r.Results.Registration_ID), r.VDOM, r.Results.Mode))
 	}
 
 	return m, true
