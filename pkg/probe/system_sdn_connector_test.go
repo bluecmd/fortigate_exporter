f76d70984988d1ee00089e461c7ddc6e45fe6cc5
Neil Schelly <neil@neilschelly.com>
2023-07-19 15:52:46 -0400

    Updating test with new description and new metric name for last_update_seconds

--- pkg/probe/system_sdn_connector_test.go
+++ pkg/probe/system_sdn_connector_test.go
@@ -21,10 +21,10 @@ func TestSystemSDNConnector(t *testing.T) {
 	# TYPE fortigate_system_sdn_connector_status gauge
 	fortigate_system_sdn_connector_status{name="AWS Infra",type="aws",vdom="root"} 3
 	fortigate_system_sdn_connector_status{name="GCP Infra",type="gcp",vdom="google"} 1
-	# HELP fortigate_system_sdn_connector_last_update Last update time for SDN connectors
-	# TYPE fortigate_system_sdn_connector_last_update gauge
-	fortigate_system_sdn_connector_last_update{name="AWS Infra",type="aws",vdom="root"} 1680708575
-	fortigate_system_sdn_connector_last_update{name="GCP Infra",type="gcp",vdom="google"} 1680708001
+	# HELP fortigate_system_sdn_connector_last_update_seconds Last update time for SDN connectors (in seconds from epoch)
+	# TYPE fortigate_system_sdn_connector_last_update_seconds gauge
+	fortigate_system_sdn_connector_last_update_seconds{name="AWS Infra",type="aws",vdom="root"} 1680708575
+	fortigate_system_sdn_connector_last_update_seconds{name="GCP Infra",type="gcp",vdom="google"} 1680708001
 	`
 
 	if err := testutil.GatherAndCompare(r, strings.NewReader(em)); err != nil {
